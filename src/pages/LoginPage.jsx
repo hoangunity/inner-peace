733bf81b61b35e3e0079c8f34e686b3e1513e0a3
Duong Minh Hoang <hoangdm.141196@gmail.com>
2023-07-26 08:01:11 +0700

    Added TailwindCss

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,67 @@
+// src/components/LoginPage.js
+
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add login logic here, e.g., authenticate the user
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-md bg-white p-8 shadow-md rounded-md">
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
